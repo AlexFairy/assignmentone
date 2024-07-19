051c596a4428db605d04f233f9c70d3b8891dca2
AlexFairy <AlexM121687@gmail.com>
2024-07-19 09:40:13 -0700

    updating pythonSyntax_assignmentOne

--- pythonSyntax_assignmentOne.py
+++ pythonSyntax_assignmentOne.py
@@ -1,4 +1,4 @@
-"""#1.Showcase Your Dance Moves!
+#1.Showcase Your Dance Moves!
 #Task 1: Code Correction Below is a piece of Python code with incorrect indentation. Your task is to correct it.
 
 weather = "sunny"
@@ -68,11 +68,12 @@ print(total_cost)
 #Task 2: Bank Interest 
 # If you have a savings account with a particular initial amount and a fixed yearly interest rate, calculate the total amount in your account after a year. 
 # So if you had $10,000 at a 7% interest write code that would tell us the amount at the end of the year. 
-# For the example the expected outcome would be $10,700."""
+# For the example the expected outcome would be $10,700.
 
 
 p = 10000
 r = .07
 
 yearly_return = p * r
+
 print(int(yearly_return + p))
